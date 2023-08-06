43fa14e4609e8884575e920bc6a30ab1d8f2b42e
AnAs21949 <AnAs21949@gmail.com>
2023-08-06 13:34:34 +0100

    depq

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   const [inputValue, setInputValue] = useState('');
   const [filteredTasks, setFilteredTasks] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [light, setLight] = useState(true)
 
 
   const handelSubmit = (e) => {
@@ -67,8 +68,8 @@ function App() {
   
   
   const handleLightning = () => {
-    setShow((prev) => !prev)
-    document.body.style.backgroundColor = `${show ?  'hsl(235, 21%, 11%)' : "#ffff"}`;
+    setLight((prev) => !prev)
+    document.body.style.backgroundColor = `${light ?  'hsl(235, 21%, 11%)' : "#ffff"}`;
 
   };
   
@@ -151,7 +152,7 @@ function App() {
       <div className="content">
         <div className="header">
           <p>TODO</p>
-          {show ? (
+          {light ? (
             <svg onClick={handleLightning} xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fillRule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"/></svg>
 
           ) :  (
